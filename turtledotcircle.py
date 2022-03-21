825b3460a54dad1115a56348bb23c78602cd8f2d
COny_Knight <97920186+COny7897@users.noreply.github.com>
2022-03-22 01:25:55 +0700

    turtledotcircle

---
+++ turtledotcircle.py
@@ -0,0 +1,20 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle
+tao = turtle.Pen()
+tao.shape('turtle')
+def dot():
+    for i in range(36):
+        tao.dot(25)
+        tao.lt(10)
+        tao.fd(75)
+
+        
+dot()
+tao.rt(90)
+dot()
+tao.
+tao.rt(90)
+dot()
+tao.rt(90)
+dot()
